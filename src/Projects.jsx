473f74daec5d51f63d29af918df6c9bf3b09584c
Nicko <nickoreorizo14@gmail.com>
2025-11-15 22:49:24 +0800

    New Project Cards

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,14 +1,20 @@
 import './css/Projects.css'
 import './css/gradients.css'
+import './css/fonts.css'
 function  Projects(){
 return(<section>
-<p className='inter-bold ts-header text-center mb-20'>Web <span className="text-gradient">Projects</span></p>
+
+<div className="project-section">
+<p className='inter-bold ts-header text-center mb-5'>Web <span className="text-[#6900ff]">Projects</span></p>
+<p className='inter-bold text-center ts-subheader'>These are the Websites that I've created by applying the skills that I've learned.</p>
 
 <div className="project-card-section">
-
-<div className="project-card">
-
+<div className="project-card"></div>
+<div className="project-card"></div>
+<div className="project-card"></div>
+<div className="project-card"></div>
 </div>
+
 
 </div>
 </section>);
